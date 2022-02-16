d066c613e0d5a5c9c3134292e6c64308f2271c7b
Brett Chandler <thunderbuck@outlook.com>
2022-02-15 17:08:43 -0700

    Resetting

---
+++ TRMDataManager/Controllers/SaleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class SaleController : ApiController
+    {
+        public void Post(TRMDataManager.Models.SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            data.SaveSale(sale, userId);
+        }
+
+        //public List<ProductModel> Get()
+        //{
+        //    ProductData data = new ProductData();
+
+        //    return data.GetProducts();
+        //}
+    }
+}
